c992128283825769e8ddbc4f800c96f442f83965
Diego Arenas <diego_arenasmo@fet.edu.co>
2024-05-28 17:31:57 -0500

    deploy new

--- index.js
+++ index.js
@@ -1,87 +1,117 @@
-// server.js
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
+const fs = require("fs");
+
+// Preparar el servidor para HTTPS (si tienes un certificado SSL/TLS)
+// const serverOptions = {
+//   key: fs.readFileSync('path/to/your/privkey.pem'),
+//   cert: fs.readFileSync('path/to/your/cert.pem')
+// };
+// const server = https.createServer(serverOptions, app);
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-let clients = [];
-let iconos = [
-  '<i class="fas fa-star"></i>',
-  '<i class="far fa-star"></i>',
-  '<i class="fas fa-star-of-life"></i>',
-  '<i class="fas fa-star-and-crescent"></i>',
-  '<i class="fab fa-old-republic"></i>',
-  '<i class="fab fa-galactic-republic"></i>',
-  '<i class="fas fa-sun"></i>',
-  '<i class="fas fa-stroopwafel"></i>',
-  '<i class="fas fa-dice"></i>',
-  '<i class="fas fa-chess-knight"></i>',
-  '<i class="fas fa-chess"></i>',
-  '<i class="fas fa-dice-d20"></i>',
-];
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+class GameState {
+  constructor() {
+    this.clients = [];
+    this.iconos = [
+      '<i class="fas fa-star"></i>',
+      '<i class="far fa-star"></i>',
+      '<i class="fas fa-star-of-life"></i>',
+      '<i class="fas fa-star-and-crescent"></i>',
+      '<i class="fab fa-old-republic"></i>',
+      '<i class="fab fa-galactic-republic"></i>',
+      '<i class="fas fa-sun"></i>',
+      '<i class="fas fa-stroopwafel"></i>',
+      '<i class="fas fa-dice"></i>',
+      '<i class="fas fa-chess-knight"></i>',
+      '<i class="fas fa-chess"></i>',
+      '<i class="fas fa-dice-d20"></i>',
+    ];
+    this.gameState = {
+      iconos: [],
+      selecciones: [],
+    };
   }
-  return array;
-}
 
-let gameState = {
-  iconos: [],
-  selecciones: [],
-};
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
-wss.on("connection", (ws) => {
-  clients.push(ws);
-  console.log("New client connected");
+  addClient(client) {
+    this.clients.push(client);
+    console.log("New client connected");
+  }
 
-  ws.on("message", (message) => {
+  removeClient(client) {
+    this.clients = this.clients.filter((c) => c !== client);
+    console.log("Client disconnected");
+  }
+
+  handleMessage(client, message) {
     const data = JSON.parse(message);
     if (data.type === "newGame") {
-      gameState.iconos = shuffle([...iconos, ...iconos]).slice(0, 24);
-      gameState.selecciones = [];
-      clients.forEach((client) => {
-        client.send(
-          JSON.stringify({ type: "initGame", iconos: gameState.iconos })
+      this.gameState.iconos = this.shuffle([
+        ...this.iconos,
+        ...this.iconos,
+      ]).slice(0, 24);
+      this.gameState.selecciones = [];
+      this.clients.forEach((c) => {
+        c.send(
+          JSON.stringify({ type: "initGame", iconos: this.gameState.iconos })
         );
       });
     } else if (data.type === "selectCard") {
-      gameState.selecciones.push(data.index);
-      clients.forEach((client) => {
-        client.send(JSON.stringify({ type: "selectCard", index: data.index }));
+      this.gameState.selecciones.push(data.index);
+      this.clients.forEach((c) => {
+        c.send(JSON.stringify({ type: "selectCard", index: data.index }));
       });
-      if (gameState.selecciones.length === 2) {
-        const [firstIndex, secondIndex] = gameState.selecciones;
-        const firstCard = gameState.iconos[firstIndex];
-        const secondCard = gameState.iconos[secondIndex];
+      if (this.gameState.selecciones.length === 2) {
+        const [firstIndex, secondIndex] = this.gameState.selecciones;
+        const firstCard = this.gameState.iconos[firstIndex];
+        const secondCard = this.gameState.iconos[secondIndex];
         if (firstCard !== secondCard) {
           setTimeout(() => {
-            clients.forEach((client) => {
-              client.send(
+            this.clients.forEach((c) => {
+              c.send(
                 JSON.stringify({
                   type: "deselectCards",
                   indices: [firstIndex, secondIndex],
                 })
               );
             });
-            gameState.selecciones = [];
+            this.gameState.selecciones = [];
           }, 1000);
         } else {
-          gameState.selecciones = [];
+          this.gameState.selecciones = [];
         }
       }
     }
-    console.log(message);
+  }
+}
+
+const gameState = new GameState();
+
+wss.on("connection", (ws) => {
+  gameState.addClient(ws);
+
+  ws.on("message", (message) => {
+    gameState.handleMessage(ws, message);
   });
 
   ws.on("close", () => {
-    clients = clients.filter((client) => client !== ws);
-    console.log("Client disconnected");
+    gameState.removeClient(ws);
+  });
+
+  ws.on("error", (error) => {
+    console.error("WebSocket error:", error);
   });
 });
 
